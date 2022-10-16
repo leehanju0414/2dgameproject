b3ea337055c3ad484dbda52fa23821bd00ce87d9
leehanju <rndrnfptb@gmail.com>
2022-10-16 19:26:11 +0900

    maked bullet class

--- main_character.py
+++ main_character.py
@@ -9,29 +9,39 @@ class Background:
     def draw(self):
         self.image.draw(300,450)
 
+class Bullet:
+    global shoot
+    def __init__(self):
+        self.x, self.y = gunner.x, gunner.y+70
+        self.blv = 'Blv1'
+        self.image = load_image('Blv1.png')
+
+    def update(self):
+        if self.y < 900:
+            self.y += 1
+
+    def draw(self):
+        if self.blv == 'Blv1' and shoot == 1:
+            self.image.draw(gunner.x, self.y)
+
 class Gunner:
-    global cdir, head, shoot
+    global head, shoot
     def __init__(self):
         self.x, self.y = 300, 30
         self.frame = 0
         self.dir = 0
         self.image = load_image('gunner_image.png')
-        self.bullet = None
-        self.bullet_image = load_image('bullet_image.png')
 
     def update(self, imgcnt):
         self.frame = (self.frame + 1) % imgcnt
 
-        if cdir == -1:
-            self.dir = -1
+        if self.dir == -1:
             if 0 < self.x - 1 < 600:
                 self.x += self.dir * 1
-        elif cdir == 1:
-            self.dir = 1
+        elif self.dir == 1:
             if 0 < self.x + 1 < 600:
                 self.x += self.dir * 1
-        elif cdir == 0:
-            self.dir = 0
+        else: self.dir = 0
 
     def draw(self):
         if self.dir == 1 and shoot == 0:
@@ -46,16 +56,16 @@ class Gunner:
             self.image.clip_draw(self.frame * 35, 150, 35, 70, self.x, self.y+10)
         elif shoot == 1 and head == 0:
             self.image.clip_draw(self.frame*35, 220, 35, 70, self.x, self.y+30)
-            self.image.clip_draw(self.frame*35, 430, 35, 35, self.x, self.y)
+            self.image.clip_draw(self.frame*35, 290, 35, 35, self.x, self.y)
         elif shoot == 1 and head == 1:
             self.image.clip_draw(self.frame*35, 360, 35, 70, self.x, self.y+30)
             self.image.clip_draw(self.frame*35, 430, 35, 35, self.x, self.y)
 
-        if self.bullet ==
-
-
 def handle_events():
-    global cdir, head, shoot, imgcnt # 머리 방향
+    global cdir, head, shoot # head=머리 방향
+
+    bullet_xy = []
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -65,42 +75,48 @@ def handle_events():
                 game_framework.quit()
             elif event.key == SDLK_LEFT:
                 head = 0 # 왼쪽
-                cdir -= 1
+                gunner.dir -= 1
             elif event.key == SDLK_RIGHT:
                 head = 1 # 오른쪽
-                cdir += 1
+                gunner.dir += 1
             elif event.key == SDLK_LCTRL:
                 shoot += 1
+            elif event.key == SDLK_1:
+                pass
+
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                cdir -= 1
+                gunner.dir -= 1
             elif event.key == SDLK_LEFT:
-                cdir += 1
+                gunner.dir += 1
             elif event.key == SDLK_LCTRL:
                 shoot -= 1
 
 gunner = None
 background = None
-cdir = 0
+bullet = None
 head = 1
 shoot = 0
 imgcnt = 8
-
 def enter():
-    global gunner, background
+    global gunner, background, bullet
     gunner = Gunner()
     background = Background()
+    bullet = Bullet()
 
 def exit():
-    global gunner, background
+    global gunner, background, bullet
     del gunner
     del background
+    del bullet
 
 def update():
     gunner.update(imgcnt)
+    bullet.update()
 
 def draw():
     clear_canvas()
     background.draw()
     gunner.draw()
+    bullet.draw()
     update_canvas()
