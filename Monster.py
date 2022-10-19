aacb1fdb815153bc7e569b5a35323900e2df6f03
hanju lee <rndrnfptb@gmail.com>
2022-10-19 17:35:47 +0900

    몬스터 모듈 준비

---
+++ Monster.py
@@ -0,0 +1,23 @@
+from pico2d import *
+import play_state
+
+class Normal_mob:
+    def __init__(self):
+        self.mlv = 'MobLv1'
+        self.image = load_image()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+class Boss_mob:
+    def __init__(self):
+        self.image = load_image()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
